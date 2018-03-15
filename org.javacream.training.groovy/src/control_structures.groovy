3756e037ab8fec1bd09c703d0c23691ef41d401c
Rainer Sawitzki <training@rainer-sawitzki.de>
2018-03-15 12:26:05 +0100

    switch for

--- org.javacream.training.groovy/src/control_structures.groovy
+++ org.javacream.training.groovy/src/control_structures.groovy
@@ -26,3 +26,48 @@ if (n1 < n2){
 	println "n1 was greater than or equal to n2"
 
 }
+
+//for (start; bedingung; increment)
+for (def i = 0; i < 10; i++){
+	println (i)
+}
+
+
+//while(bedingung)
+def counter = 0
+
+while (counter < 5){
+	println counter
+	counter++
+}
+
+
+def name = "Hugo"
+
+switch (name){
+	case "Emil":
+		println "name was Emil"
+		break
+	case "Egon":
+		println "name was Egon"
+		break
+	case "Hugo":
+		println "name was Hugo"
+		break
+	default:
+		println "unknown name"
+}
+
+
+
+for (def j = 0; j < 10; j++){
+	if (j < 5){
+		println "smaller than 5"
+	}else{
+		println "greater than 5"
+
+	}
+}
+
+
+
