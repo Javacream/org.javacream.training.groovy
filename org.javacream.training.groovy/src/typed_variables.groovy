b1d863bc55ade3cfe85df726438a0a9750c17ff8
Rainer Sawitzki <training@rainer-sawitzki.de>
2018-03-15 11:48:09 +0100

    working

---
+++ org.javacream.training.groovy/src/typed_variables.groovy
@@ -0,0 +1,33 @@
+//Zeilenkommentar
+
+/*
+ * Einführung von Variablen und Literalen 
+ */
+
+//def <variablen_name> = Literal
+
+
+String output
+
+
+String  greeting = "Hello" //"..." : Zeichenketten-Literal, String
+Integer number = 42 //42 Ganzzahl-Literal
+Double realNumber = 47.11 //Kommazahl-Literal
+Boolean decision = true //true | false: Logische Literal
+
+/*
+print message //Ausgabe der Variable message
+print " "
+print number
+print " "
+print realNumber
+println ""
+
+*/
+output =  "message=${greeting}, Number=${number}, "
+println output
+
+//Wertzuweisung an eine vorhandene Variable, kein def notwendig
+output = "Hallo"
+println output
+
