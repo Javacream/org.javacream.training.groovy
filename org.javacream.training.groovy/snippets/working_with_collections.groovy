9f83aec77a15e6a297f47ba7de5757602ddcb38b
Rainer Sawitzki <training@rainer-sawitzki.de>
2018-03-16 10:37:31 +0100

    working

--- org.javacream.training.groovy/snippets/working_with_collections.groovy
+++ org.javacream.training.groovy/snippets/working_with_collections.groovy
@@ -1,2 +1,24 @@
 List elements = ["A", "C", "B", "AA", "Z1", "FF"]
+Map states = ["München": "Bayern", "Stuttgart": "BW", "Augsburg": "Bayern"]
+ 
+for (String element in elements){
+	println element
+}
 
+for (def element in states){
+	println element
+}
+println ("#################")
+
+for (String key in states.keySet()){
+	print key
+	print "::"
+	def value = states.get(key)
+	print value
+	println "${key} :: ${states.get(key)}"
+}
+println ("#################")
+
+for (String value in states.values()){
+	println value
+}
---
+++ org.javacream.training.groovy/snippets/working_with_files.groovy
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+
+String number1String = JOptionPane.showInputDialog("Please enter nr1:")
+String number2String = JOptionPane.showInputDialog("Please enter nr2:")
+
+
+
+def number1 = number1String.toInteger()
+def number2 = number2String.toInteger()
+
+
+println number1 + number2
+
