1ff0ac827ce3c7d0f2c1cce79ac344c5f6dcc02d
Rainer Sawitzki <trainingrainer-sawitzki.de>
2022-01-24 13:46:43 +0100

    play around

--- snippets/HelloWorldDetailed.groovy
+++ snippets/HelloWorldDetailed.groovy
@@ -30,3 +30,12 @@ def off = false
     //Innerhalb einer Zeichenkette kann auf sichtbare Variablen mit ${...} zugegriffen werden
 def output = "Greeting=${greeting}, number=${number}"
 println output
+//Laufzeitfehler:
+//println outpu
+
+//WARNUNG: Das folgende scheint zu funktionieren, ist aber schlechtester Stil und funktioniert auch nur in dieser speziellen Situtaion
+undeclared_output = "Test"
+println undeclared_output
+//Groovy ist aktuell eine untypisierte Sprache
+output = 4711
+println output
