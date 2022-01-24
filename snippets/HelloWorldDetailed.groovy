a9895b4976018b53ba23dfd89d8b68ae143dc16f
Rainer Sawitzki <trainingrainer-sawitzki.de>
2022-01-24 13:31:51 +0100

    accessing variables

--- snippets/HelloWorldDetailed.groovy
+++ snippets/HelloWorldDetailed.groovy
@@ -22,3 +22,11 @@ def realNumber = 4.2
 
 def on = true
 def off = false
+
+//Zugriff auf Variablen zur Ausgabe
+
+    //println ("Greeting=" + greeting + ", number=" + number)
+
+    //Innerhalb einer Zeichenkette kann auf sichtbare Variablen mit ${...} zugegriffen werden
+def output = "Greeting=${greeting}, number=${number}"
+println output
