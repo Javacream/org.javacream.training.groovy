4d70e98c5adbf2c54558a227aa96640bc804cd3a
Rainer Sawitzki <trainingrainer-sawitzki.de>
2022-01-24 13:20:58 +0100

    add variables

---
+++ snippets/HelloWorldDetailed.groovy
@@ -0,0 +1,24 @@
+//Ein erstes Skript mit Konsolen-Ausgabe
+/*
+    Ein Mehrzeiliger Kommentar
+*/
+
+//Syntactic Sugar
+
+println "HelloWorld"
+
+//Eigentlich
+println("HelloWorld");
+
+//Arbeiten mit Variablen
+
+//def <variablen-name> = Wert/Literal
+def greeting = "HelloWorld"
+println greeting
+
+def number = 42
+
+def realNumber = 4.2
+
+def on = true
+def off = false
