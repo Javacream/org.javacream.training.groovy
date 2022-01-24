48f6f25f735990bb6d3569656cc2dd381fa177d4
Rainer Sawitzki <trainingrainer-sawitzki.de>
2022-01-24 16:50:06 +0100

    add streaming example

--- snippets/CollectionsDemo.groovy
+++ snippets/CollectionsDemo.groovy
@@ -14,6 +14,20 @@ def mapDemo(){
     println(postalCodesToCity.size())
 }
 
+def data(){
+    def names = ["Emil", "Hugo", "Fritz", "Eduard", "Zvonimir", "Alexander"]
+    names.forEach(name -> println(name))
+    println("---")
+    names.stream().filter(name -> name.startsWith("E")).forEach(name -> println(name))
+    println("---")
+    names.stream().filter(name -> name.startsWith("E")).map(name -> name.toUpperCase()).forEach(name -> println(name))
+    println("---")
+    names.stream().filter(name -> name.startsWith("E")).map(name -> name.length()).forEach(name -> println(name))
+    println("---")
+    println(names.stream().filter(name -> name.startsWith("E")).collect())
 
-listDemo()
-mapDemo()
+}
+
+//listDemo()
+//mapDemo()
+data()
