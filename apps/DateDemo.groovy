f1e26250d8986011f79ec4543263ef1202e46c72
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-01-25 12:04:13 +0100

    introduce date example

---
+++ apps/DateDemo.groovy
@@ -0,0 +1,12 @@
+class DateDemo{
+    def static main(args) {
+     def actualDate = new Date()
+     println(actualDate)
+     def date = Date.parse("dd.MM.yyyy", "19.05.2001")
+     println(date)
+     println(date.format("dd/MM/yyyy"))
+     println(date + 5)
+     println(date.plus(3))
+     println(date.getMonth()) 
+    }
+}
